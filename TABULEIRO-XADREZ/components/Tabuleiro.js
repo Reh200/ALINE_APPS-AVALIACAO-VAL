1ba8d5618fcf4663c6ed21361c7bc11bd7b22c9f
Reh200 <alinerenata1400@gmail.com>
2025-04-16 00:36:23 -0300

    commit

--- TABULEIRO-XADREZ/components/Tabuleiro.js
+++ TABULEIRO-XADREZ/components/Tabuleiro.js
@@ -1,42 +1,59 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import Quadrado from './Quadrado';
 
-const Tabuleiro = () => {
-  const renderTabuleiro = () => {
-    let quadrados = [];
-    let corAlternada = true;
+export default function Tabuleiro() {
+  return (
+    <View style={styles.tabuleiro}>
+      {/* Linha 1 */}
+      <View style={styles.linha}>
+        <View style={[styles.quadrado, styles.branco]} />
+        <View style={[styles.quadrado, styles.preto]} />
+        <View style={[styles.quadrado, styles.branco]} />
+        <View style={[styles.quadrado, styles.preto]} />
+      </View>
 
-    for (let i = 0; i < 4; i++) {
-      let linha = [];
-      for (let j = 0; j < 4; j++) {
-        linha.push(
-          <Quadrado key={`${i}-${j}`} cor={corAlternada} />
-        );
-        corAlternada = !corAlternada;
-      }
-      quadrados.push(
-        <View key={i} style={styles.linha}>
-          {linha}
-        </View>
-      );
-      corAlternada = !corAlternada;
-    }
-    return quadrados;
-  };
+      {/* Linha 2 */}
+      <View style={styles.linha}>
+        <View style={[styles.quadrado, styles.preto]} />
+        <View style={[styles.quadrado, styles.branco]} />
+        <View style={[styles.quadrado, styles.preto]} />
+        <View style={[styles.quadrado, styles.branco]} />
+      </View>
 
-  return <View style={styles.container}>{renderTabuleiro()}</View>;
-};
+      {/* Linha 3 */}
+      <View style={styles.linha}>
+        <View style={[styles.quadrado, styles.branco]} />
+        <View style={[styles.quadrado, styles.preto]} />
+        <View style={[styles.quadrado, styles.branco]} />
+        <View style={[styles.quadrado, styles.preto]} />
+      </View>
+
+      {/* Linha 4 */}
+      <View style={styles.linha}>
+        <View style={[styles.quadrado, styles.preto]} />
+        <View style={[styles.quadrado, styles.branco]} />
+        <View style={[styles.quadrado, styles.preto]} />
+        <View style={[styles.quadrado, styles.branco]} />
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  tabuleiro: {
+    marginTop: 10,
   },
   linha: {
     flexDirection: 'row',
   },
+  quadrado: {
+    width: 60,
+    height: 60,
+  },
+  branco: {
+    backgroundColor: '#fff',
+  },
+  preto: {
+    backgroundColor: '#000',
+  },
 });
-
-export default Tabuleiro;
